1e9ecd2e1faf3c1b752905f851abc6be1f8d0255
aqadavinash <avinash@aqad.ae>
2024-11-30 19:49:39 +0530

    add

--- dbconn.php
+++ dbconn.php
@@ -1,11 +1,11 @@
 <?php 
-// $databaseHost = 'mydb.cj2aycaoyu63.us-west-1.rds.amazonaws.com';
-// $databaseName = 'tpeg';
-// $databaseUsername = 'admin';
-// $databasePassword = 'Luxyaragroup';
+$databaseHost = 'mydb.cj2aycaoyu63.us-west-1.rds.amazonaws.com';
+$databaseName = 'tpeg';
+$databaseUsername = 'admin';
+$databasePassword = 'Luxyaragroup';
  
-$databaseHost = 'localhost';
-$databaseName = 'tpeg1';
-$databaseUsername = 'root';
-$databasePassword = 'root@123';
+// $databaseHost = 'localhost';
+// $databaseName = 'tpeg1';
+// $databaseUsername = 'root';
+// $databasePassword = 'root@123';
 ?>
