f0de56cc1603b044dce51cdc6d1b0d115e7c4a4b
aqadavinash <avinash@aqad.ae>
2024-12-14 14:47:28 +0530

    add

--- signup.php
+++ signup.php
@@ -58,9 +58,9 @@
   <!-- Signup Button -->
   <button type="submit" name="submit" class="w-full text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">✍️ Sign Up</button>
 
-  <!-- Already Registered -->
+  <!-- Already Registered11 -->
   <div class="text-sm font-medium text-gray-500 text-center">
-    Already registered? <a href="login" class="text-blue-700 hover:underline">Login here</a>
+    Already registered.? <a href="login" class="text-blue-700 hover:underline">Login here</a>
   </div>
 </form>
 
